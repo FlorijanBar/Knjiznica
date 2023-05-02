3902ed194bba2688d1410a1c6d0f735316747466
fbara <fbara@DESKTOP-6QE881M>
2023-05-02 10:14:12 +0200

    prvi proba komit

---
+++ Knjiznica/src/main/java/com/example/knjiznica/KnjiznicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.knjiznica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnjiznicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnjiznicaApplication.class, args);
+	}
+
+}
---
+++ Knjiznica/src/test/java/com/example/knjiznica/KnjiznicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knjiznica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnjiznicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
